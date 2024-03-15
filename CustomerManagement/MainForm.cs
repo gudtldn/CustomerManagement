5988a51dd6b20a137eb78eb1311623359ac5314e
gudtldn <gudtldn1234@gmail.com>
2024-03-15 16:53:59 +0900

    고객 목록 및 의류 정보를 MainForm 로드 시에 표시하도록 수정함

--- CustomerManagement/MainForm.cs
+++ CustomerManagement/MainForm.cs
@@ -19,5 +19,33 @@ namespace CustomerManagement
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            DataSQL data = new DataSQL();
+            foreach (Customer customer in data.GetAllCustomers())
+            {
+                ListViewItem item = new ListViewItem(customer.Name);
+                item.SubItems.Add(Utils.FormatPhoneNumber(customer.PhoneNumber));
+                item.Tag = customer;
+                CustomerListView.Items.Add(item);
+            }
+
+            if (CustomerListView.Items.Count > 0)
+            {
+                CustomerListView.Items[0].Selected = true;
+                foreach (Garment garment in data.GetGarments((Customer)CustomerListView.Items[0].Tag))
+                {
+                    ListViewItem item = new ListViewItem(garment.ReceptionDate.ToString());
+                    item.SubItems.Add(garment.ProcessingDate.ToString());
+                    item.SubItems.Add(garment.IsCompleted ? "완료" : "미완료");
+                    item.SubItems.Add(garment.Contents);
+                    item.SubItems.Add($"{garment.Price:n0} 원");
+                    item.SubItems.Add(garment.Note);
+                    item.Tag = garment;
+                    GarmentListView.Items.Add(item);
+                }
+            }
+        }
     }
 }
