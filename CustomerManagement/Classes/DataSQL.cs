55a64b0c2b0b3fda44d18c59c7ee99d10aadef54
gudtldn <gudtldn1234@gmail.com>
2024-03-15 16:53:52 +0900

    데이터베이스 파일이 없을 경우 생성하고 테이블을 추가하는 로직을 추가했습니다. 고객 정보와 관련된 쿼리를 추가하고, 고객 및 의류 정보를 조회하는 메서드를 추가했습니다.

--- CustomerManagement/Classes/DataSQL.cs
+++ CustomerManagement/Classes/DataSQL.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
+using System.IO;
 using System.Runtime.Remoting.Messaging;
 
 
@@ -16,6 +17,40 @@ namespace CustomerManagement.Classes
         public DataSQL(string path = "./data/customer.db")
         {
             database_path = $"Data Source={path};";
+            FileInfo info = new FileInfo(path);
+
+            // DB 파일이 없으면 생성
+            if (!info.Exists)
+            {
+                SQLiteConnection.CreateFile(path);
+                using (SQLiteConnection connection = new SQLiteConnection(database_path))
+                {
+                    connection.Open();
+                    string create_table_query = @"
+                        CREATE TABLE `customers` (
+                            `id`	INTEGER,
+                            `name`	TEXT NOT NULL UNIQUE,
+                            `phone_number`	TEXT NOT NULL UNIQUE,
+                            PRIMARY KEY(`id` AUTOINCREMENT)
+                        );
+                        CREATE TABLE `garments` (
+                            `id`	INTEGER,
+                            `reception_date`	TEXT,
+                            `processing_date`	TEXT,
+                            `is_completed`	INTEGER NOT NULL,
+                            `contents`	TEXT,
+                            `price`	INTEGER NOT NULL,
+                            `note`	TEXT,
+                            `customer_id`	INTEGER NOT NULL,
+                            FOREIGN KEY(`customer_id`) REFERENCES `customers`(`id`),
+                            PRIMARY KEY(`id` AUTOINCREMENT)
+                        )";
+                    using (SQLiteCommand create_table_command = new SQLiteCommand(create_table_query, connection))
+                    {
+                        create_table_command.ExecuteNonQuery();
+                    }
+                }
+            }
         }
 
         public IEnumerable<SQLiteDataReader> GetData(string command, params object[] args)
@@ -41,11 +76,81 @@ namespace CustomerManagement.Classes
             }
         }
 
-        public IEnumerable<CustomerInfo> GetAllCustomers()
+        public Customer? GetCustomer(string name, string phone_number)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(database_path))
+            {
+                connection.Open();
+                using (SQLiteCommand sql_command = new SQLiteCommand("SELECT * FROM customers WHERE name = @name AND phone_number = @phone_number", connection))
+                {
+                    sql_command.Parameters.AddWithValue("@name", name);
+                    sql_command.Parameters.AddWithValue("@phone_number", phone_number);
+                    using (SQLiteDataReader reader = sql_command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new Customer(Convert.ToInt32(reader["id"]), reader["name"].ToString(), reader["phone_number"].ToString());
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+        }
+
+        public Customer? GetCustomerFromName(string name)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(database_path))
+            {
+                connection.Open();
+                using (SQLiteCommand sql_command = new SQLiteCommand("SELECT * FROM customers WHERE name = @name", connection))
+                {
+                    sql_command.Parameters.AddWithValue("@name", name);
+                    using (SQLiteDataReader reader = sql_command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new Customer(Convert.ToInt32(reader["id"]), reader["name"].ToString(), reader["phone_number"].ToString());
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+        }
+
+        public Customer? GetCustomerFromPhoneNumber(string phone_number)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(database_path))
+            {
+                connection.Open();
+                using (SQLiteCommand sql_command = new SQLiteCommand("SELECT * FROM customers WHERE phone_number = @phone_number", connection))
+                {
+                    sql_command.Parameters.AddWithValue("@phone_number", phone_number);
+                    using (SQLiteDataReader reader = sql_command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new Customer(Convert.ToInt32(reader["id"]), reader["name"].ToString(), reader["phone_number"].ToString());
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<Customer> GetAllCustomers()
         {
             foreach (SQLiteDataReader reader in GetData("SELECT * FROM customers"))
             {
-                yield return new CustomerInfo(Convert.ToInt32(reader["id"]), reader["name"].ToString(), reader["phone_number"].ToString());
+                yield return new Customer(Convert.ToInt32(reader["id"]), reader["name"].ToString(), reader["phone_number"].ToString());
             }
         }
 
@@ -58,6 +163,23 @@ namespace CustomerManagement.Classes
                 {
                     return Convert.ToInt32(sql_command.ExecuteScalar());
                 }
+            }
+        }
+
+        public IEnumerable<Garment> GetGarments(Customer customer)
+        {
+            foreach (SQLiteDataReader reader in GetData("SELECT * FROM garments WHERE customer_id = @p0", customer.ID))
+            {
+                yield return new Garment(
+                    Convert.ToInt32(reader["id"]),
+                    reader["reception_date"] as DateTime?,
+                    reader["processing_date"] as DateTime?,
+                    Convert.ToBoolean(reader["is_completed"]),
+                    reader["contents"].ToString(),
+                    Convert.ToInt32(reader["price"]),
+                    reader["note"].ToString(),
+                    customer.ID
+                );
             }
         }
     }
