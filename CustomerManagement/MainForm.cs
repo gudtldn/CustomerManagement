ac97edd068a24544db3725a3dad99f51f23442b9
gudtldn <gudtldn1234@gmail.com>
2024-03-17 19:08:21 +0900

    고객 선택코드 최적화

--- CustomerManagement/MainForm.cs
+++ CustomerManagement/MainForm.cs
@@ -37,11 +37,12 @@ namespace CustomerManagement
 
         private void CustomerListView_SelectedIndexChanged(object sender, EventArgs e)
         {
+            if (CustomerListView.SelectedItems.Count > 1) return; // 선택된 고객이 2명 이상이면 함수를 종료
+
             GarmentListView.Items.Clear(); // 선택된 고객의 의류정보를 가져오기 전에 리스트뷰를 비움
             Customer_Name_Label.Text = ""; // 선택된 고객이 없으면 라벨을 초기화
 
-            if (CustomerListView.SelectedItems.Count == 0) return; // 선택된 고객이 없으면 아무것도 하지 않음
-
+            if (CustomerListView.SelectedItems.Count == 0) return; // 선택된 고객이 없으면 함수를 종료
 
             // DB에서 선택된 고객의 의류정보를 가져와서 GarmentListView에 추가
             DataSQL data = new DataSQL();
