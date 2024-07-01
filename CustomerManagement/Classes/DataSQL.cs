22b60bf27c20786ccc5eda8c27da395f3e792a50
gudtldn <gudtldn1234@gmail.com>
2024-07-01 14:35:14 +0900

    사용하지 않는 using문 제거

--- CustomerManagement/Classes/DataSQL.cs
+++ CustomerManagement/Classes/DataSQL.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.SQLite;
 using System.IO;
-using System.Runtime.Remoting.Messaging;
+using System.Text;
 
 
 namespace CustomerManagement.Classes
