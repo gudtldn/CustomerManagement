bef07cfd7c719e028b0fe1909d05d5b5ff752c25
gudtldn <gudtldn1234@gmail.com>
2024-07-01 20:52:18 +0900

    chore: Update program startup to prevent multiple instances

--- CustomerManagement/Program.cs
+++ CustomerManagement/Program.cs
@@ -1,19 +1,55 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace CustomerManagement
 {
     static class Program
     {
+        [DllImport("user32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        static readonly Mutex mutex = new Mutex(true, "CustomerManagement");
+
         /// <summary>
         /// 해당 애플리케이션의 주 진입점입니다.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainForm());
+            if (mutex.WaitOne(TimeSpan.Zero, true))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new MainForm());
+                mutex.ReleaseMutex();
+            }
+            else
+            {
+                SetForegroundWindow();
+            }
+        }
+
+        private static void SetForegroundWindow()
+        {
+            Process current = Process.GetCurrentProcess();
+            Process[] processes = Process.GetProcessesByName(current.ProcessName);
+
+            foreach (Process process in processes)
+            {
+                if (process.Id != current.Id)
+                {
+                    IntPtr hWnd = process.MainWindowHandle;
+                    ShowWindow(hWnd, 9); // SW_RESTORE
+                    SetForegroundWindow(hWnd);
+                    break;
+                }
+            }
         }
     }
 }
